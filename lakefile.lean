b45edd499a25955953c7a4995b5af76fc89592c4
sdiehl <stephen.m.diehl@gmail.com>
2026-02-02 16:55:02 +0000

    bump version

--- lakefile.lean
+++ lakefile.lean
@@ -2,7 +2,7 @@ import Lake
 open Lake DSL
 
 package «list-utils» where
-  version := v!"1.3.0"
+  version := v!"1.4.0"
   leanOptions := #[
     ⟨`autoImplicit, false⟩,
     ⟨`relaxedAutoImplicit, false⟩
